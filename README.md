c5afdf50537bf7dbaee83d196b9318079f99498f
Gergely Imreh <imrehg@gmail.com>
2018-11-20 18:04:33 +0000

    make day numbers adjustable
    Signed-off-by: Gergely Imreh <imrehg@gmail.com>

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Export event from Mixpanel by device UUID.
 
 - Set `MIXPANEL_API_KEY` and `MIXPANEL_API_SECRET` in the environment, or in `.env`
 - Run `npm run report`
-- Change the number of devices reported by setting `MAXCOUNT`, that is 30 by default
+- Change the number of devices reported by setting `MAXCOUNT`, which is 30 by default
+- Change the number of days to sum over by setting `DAYS`, which is 3 by default
--- index.js
+++ index.js
@@ -18,26 +18,34 @@ var panel = new MixpanelExport({
   api_secret: process.env.MIXPANEL_API_SECRET
 });
 
-var thisdate = moment().format("YYYY-MM-DD");
+var days = process.env.DAYS || 3;
+var now = moment();
+var to_date = now.format("YYYY-MM-DD");
+var from_date = now.subtract(days, "days").format("YYYY-MM-DD");
 var defaultLimit = process.env.MAXCOUNT || 30;
 
 var getReport = function(event) {
   panel
     .segmentation({
-      from_date: thisdate,
-      to_date: thisdate,
+      from_date: from_date,
+      to_date: to_date,
       event: event,
       on: 'properties["uuid"]',
       limit: defaultLimit
     })
     .then(function(data) {
-      console.log(`Event: ${event}`.bgGreen);
+      console.log(`Event: ${event} / ${days} day(s)`.bgGreen);
       if (data.error) {
         console.log(`${data.error.red}\n`);
       } else {
         var devicelist = _(data.data.values)
           .toPairs()
-          .map(kvArray => [kvArray[0], kvArray[1][thisdate]])
+          .map(kvArray => [
+            kvArray[0],
+            _(kvArray[1])
+              .values()
+              .sum()
+          ])
           .sortBy(kvArray => kvArray[1])
           .reverse()
           .value();
